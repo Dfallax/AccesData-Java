607302a374b37884ce64f2f7e0b96f85c393c42c
Dfallax <dfalla0001@gmail.com>
2023-11-03 00:01:22 +0100

    repaso de acceso a datos

---
+++ Ejercicios preExamen/src/main/Compara.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Compara {
+
+	public static void main(String[] args) {
+		String path1 = "archivo1.txt";
+		String path2 = "archivo2.txt";
+		boolean iguales = true;
+		List<String> lineasArchivo1;
+		List<String> lineasArchivo2;
+		int menosLineas;
+
+		try {
+			//lee todo el archivo y lo guarda en la List
+			lineasArchivo1 = Files.readAllLines(Path.of(path1));
+			lineasArchivo2 = Files.readAllLines(Path.of(path2));
+//toma el numero de lineas que conciden 
+			menosLineas = Math.min(lineasArchivo1.size(), lineasArchivo2.size());
+			System.out.println(menosLineas);
+			
+			for (int i = 0; i < menosLineas; i++) {
+				
+				if (!lineasArchivo1.get(i).equals(lineasArchivo2.get(i))) {
+					iguales = false;
+					break;
+				}
+
+			}
+
+			if (iguales)
+				System.out.println("Son iguales");
+			else
+				System.err.println("Son distintos");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- Ejercicios preExamen/src/main/Ejercicio_2.java
+++ Ejercicios preExamen/src/main/Ejercicio_2.java
@@ -3,25 +3,34 @@ package main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
+import java.io.PrintWriter;
 
 public class Ejercicio_2 {
 
 	public static void main(String[] args) {
 
+		// Haz un programa que lea un archivo y cree otro con el mismo nombre y
+		// "_cifrado"
+		// al final que encripte el texto original. La tabla de cifrado es la que está
+		// debajo.
+		// Los caracteres que no sean los de la tabla se ignoran. Después, si el archivo
+		// termina en
+		// "_cifrado", que la aplicación lo detecte y lo descifre, mostrándonos por
+		// consola el mensaje verdadero.
 
-		//Haz un programa que lea un archivo y cree otro con el mismo nombre y "_cifrado"
-		//al final que encripte el texto original. La tabla de cifrado es la que está debajo. 
-		//Los caracteres que no sean los de la tabla se ignoran. Después, si el archivo termina en
-		//"_cifrado", que la aplicación lo detecte y lo descifre, mostrándonos por consola el mensaje verdadero.
-		
-	
 		File archivo = new File("Ejercicio 2.txt");
-	File cifrado = new File("Ejercicio 2_cifrado.txt");
-	
-		if(!archivo.exists() && !cifrado.exists()) {
+		File cifrado = new File("Ejercicio 2_cifrado.txt");
+		String decifrado="";
+		final char CARACTERES[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P',
+				'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+		final char CIFRADO[] = { '0', '$','2','&','@','1','?', ':', '=', '6', '/', '3', 'B', 'C', ')', 'E',
+				'4', 'G', 'H', 'I', '5', 'K', '9', '8','(', '7' };
+
+		if (!archivo.exists() && !cifrado.exists()) {
 			try {
 				archivo.createNewFile();
 				cifrado.createNewFile();
@@ -29,47 +38,89 @@ public class Ejercicio_2 {
 				e.printStackTrace();
 			}
 		}
-		
-		
+
 		try {
 			InputStream is = new FileInputStream(archivo);
+			
+			FileWriter fr = new FileWriter(cifrado);	
+			PrintWriter pw = new PrintWriter(fr, true);
 			long size = archivo.length();
 			int byteLeido;
-			
-			while(size != 0) {
-				
+			String cadena = "";
+
+			boolean saltoLinea = false;
+			while (size != 0) {
+				byteLeido = is.read();
+
+				if (byteLeido == 13) {
+					size--;
 					byteLeido = is.read();
-				 System.out.println((char) byteLeido);
-				 
-				 size--;
+
+					if (byteLeido == 10) {
+						cadena += "\n";
+						size++;
+						saltoLinea = true;
+					}
+				}
+				
+				if (!saltoLinea) {
+					
+					for (int i = 0; i < CARACTERES.length; i++) {
+						if ((char) byteLeido == CARACTERES[i]) {
+							cadena += CIFRADO[i];
+							break;
+						}
+					}
+					
+				}
+				saltoLinea=false;
+				size--;
 			}
-			
+			pw.println(cadena);
 			is.close();
+			fr.close();
+			pw.close();
+			cadena="";
 			
+			InputStream is_decifrar = new FileInputStream(cifrado);
+			size = cifrado.length();
+			boolean caracterDoble=false;
+			
+			while (size != 0) {
+				byteLeido = is_decifrar.read();
+
+				if (byteLeido == 13) {
+					size--;
+					byteLeido = is_decifrar.read();
+
+					if (byteLeido == 10) {
+						cadena += "\n";
+						size++;
+						saltoLinea = true;
+					}
+				}
+				
+				if (!saltoLinea || !caracterDoble) {
+					
+					for (int i = 0; i < CIFRADO.length; i++) {
+							cadena += CARACTERES[i];
+							break;
+						
+					}
+					
+				}
+				saltoLinea=false;
+				size--;
+			}
+			is_decifrar.close();
 		} catch (FileNotFoundException e) {
 
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-		try {
-			InputStream is = new FileInputStream(archivo);
-			byte[] bytesLeidos = new byte[(int) archivo.length()];
-
-			is.read(bytesLeidos);
-
-			System.out.println(Arrays.toString(bytesLeidos));
-			System.out.println(new String(bytesLeidos));
-
-			System.out.println(is.read());
-
-			is.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		
 		
 		
 	}
